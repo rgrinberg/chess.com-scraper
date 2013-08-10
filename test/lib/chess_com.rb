c3b073052a7a79e20f5ccf9b0cb8a01d441a3795
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-08-10 19:45:27 -0400

    cleaned up tests

--- test/lib/chess_com.rb
+++ test/lib/chess_com.rb
@@ -10,19 +10,27 @@ end
 describe ChessCom::GameListing do
   before do
     @user = 'rgrinberg'
+    @game = ChessCom::GameListing.new(username: @user)
   end
   it 'can fetch a game listing' do
-    listing = ChessCom::GameListing.new(username: @user)
-    (listing.games.size > 0).must_equal true
+    (@game.games.size > 0).must_equal true
   end
   it 'can detect multiple pages' do
-    listing = ChessCom::GameListing.new(username: @user)
-    (listing.pages.size > 0).must_equal true
+    (@game.pages.size > 0).must_equal true
+  end
+
+  it 'returns pages which are integers' do
+    @game.pages.each do |page|
+      assert page.is_a? Integer
+    end
+  end
+
+  it 'does not include its own page' do
+    refute @game.pages.include? @game.page
   end
   it 'can detect other players' do
-    listing = ChessCom::GameListing.new(username: @user)
-    (listing.players.size > 0).must_equal true
-    refute listing.players.include? listing.username
+    (@game.players.size > 0).must_equal true
+    refute @game.players.include? @game.username
   end
 end
 
